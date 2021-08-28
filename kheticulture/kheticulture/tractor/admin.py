c51ff877997a0ed0ea4e593665563e48a9b175f0
nandinichhajed <nandinichhajed08@gmail.com>
2021-08-28 20:52:38 +0530

    tractor

--- kheticulture/kheticulture/tractor/admin.py
+++ kheticulture/kheticulture/tractor/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Tractor,Tractor_Types,Tractor_Sub_Types_Activities
+from .models import Tractor
+from .models import Attachments
+from .models import Tractor_Attachments
+from .models import Tractor_Types
+from .models import Tractor_Sub_Types_Activities
 from store.models import (
     ProductImage,
     
 )
+admin.site.register(Tractor)
+admin.site.register(Attachments)
+admin.site.register(Tractor_Attachments)
 admin.site.register(Tractor_Types)
 admin.site.register(Tractor_Sub_Types_Activities)
-admin.site.register(Tractor)
 
 
